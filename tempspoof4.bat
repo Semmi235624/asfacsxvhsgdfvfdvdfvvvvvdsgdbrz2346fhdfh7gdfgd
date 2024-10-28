5795fe4e164053c83a5ac79f28f7232013504caa
Semmi235624 <166162605+Semmi235624@users.noreply.github.com>
2024-10-28 14:42:18 +0100

    Add files via upload

---
+++ tempspoof4.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: Set the path to the executable
+set "exePath=C:\Program Files\Snippingtool\mapper.exe"
+set "driver1=bqnl1y.sys"
+set "data=.data"
+
+:: Change to the directory where the executable is located
+cd /d "C:\Program Files\Snippingtool"
+
+
+exit
